7f0316a83dd4ed5485bea30c43251d544fb85637
flyingrub <flyinggrub@gmail.com>
2016-02-09 17:29:43 +0100

    Cleanup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import americano from 'americano';
 // Define the function to boot the application's HTTP server.
 export const boot = function boot(callback) {
     const options = {
-        name: 'tasky',
+        name: 'cozic',
         root: __dirname,
         port: process.env.PORT || 9250,
         host: process.env.HOST || '127.0.0.1',
