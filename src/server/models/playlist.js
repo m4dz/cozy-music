9abb5097a56e0cfadda1e971acc98bbaa2e17632
flyingrub <flyinggrub@gmail.com>
2016-02-10 15:39:56 +0100

    Add Model for tracks and playlists

---
+++ src/server/models/playlist.js
@@ -0,0 +1,12 @@
+// See documentation on https://github.com/cozy/cozy-db
+
+import cozydb from 'cozydb'
+
+export const Track = cozydb.getModel('Playlist', {
+    title: String,
+    tracks: Object,
+    dateAdded: {
+        type: Date, 
+        default: Date.now
+    }
+});
---
+++ src/server/models/track.js
@@ -0,0 +1,23 @@
+// See documentation on https://github.com/cozy/cozy-db
+
+import cozydb from 'cozydb'
+
+export const Track = cozydb.getModel('Track', {
+    title: String,
+    artist: String,
+    album: String,
+    year: String,
+    genre: String,
+    duration: String,
+    playlists: Object,
+    dateAdded: {
+        type: Date, 
+        default: Date.now
+    },
+    lastPlayed: Date,
+    plays: {
+        type: Number,
+        default: 0
+    },
+    binary: Object
+});
